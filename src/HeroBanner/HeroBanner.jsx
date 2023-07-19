18ec8d4c834bd9712c9488fc57d0bc7cd02dc588
jtompkins415 <john.tompkins415@gmail.com>
2023-07-18 22:16:02 -0700

    remove image from component

--- src/HeroBanner/HeroBanner.jsx
+++ src/HeroBanner/HeroBanner.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-
+import './HeroBanner.css';
 
 function HeroBanner(){
     
     return (
         <div className='hero-container'>
             <div className='hero-title'>
-                <h1>John Tompkins</h1>
+                <h1>John Tompkins.</h1>
                 <h2>Web Developer</h2> 
-            </div>
-            <div className='hero-image-container'>
-                <img src='src/assets/newProfPic.jpg' alt='profile-picture'/>
-            </div>
-            
+            </div>    
         </div>
     )
 };
