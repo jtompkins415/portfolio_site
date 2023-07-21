67acf540683ad4786dc0eeddfda4e638cc39c547
jtompkins415 <john.tompkins415@gmail.com>
2023-07-20 22:00:40 -0700

    create array module for project objects

---
+++ src/ProjectContainer/projectObjectArray.js
@@ -0,0 +1,15 @@
+export const projectObjectArray = [
+    {
+        name: 'LOCAL PROVISIONS',
+    description: 'LOCAL PROVISIONS is an single page application, inspired by EATER/THRILLIST, that allows users to discover new restaurants, bars, and specialty food/beverage shops. A user can register for the site, explore places of interest in different regions, and save them to be referenced later.',
+    techStack: ['React', 'Bootstrap', 'Express.js', 'PostgreSQL' ],
+    imgSrc: 'src/assets/project-pics/localProvisions.jpeg',
+    url: 'https://github.com/jtompkins415/capstone-local-provisions'},
+    {
+        name: 'PROVISIONS: Online Storefront',
+        description: 'PROVISIONS is an online platform providing users access to a highly currated selection of craft beer, natural wine, N/A beverages, and specialty food items.',
+        techStack: ['Flask', 'SQLAlchemy', 'Jinja', 'WTForms', 'PostgreSQL'],
+        imgSrc: '',
+        url: 'https://github.com/jtompkins415/capstone-1-provisions'
+    }
+]
