5989af8922616aa21d70064fa9f61754af72c744
jtompkins415 <john.tompkins415@gmail.com>
2023-07-20 21:59:28 -0700

    Remove function and add style for "divider bar"

--- src/HeroBanner/HeroBanner.jsx
+++ src/HeroBanner/HeroBanner.jsx
@@ -1,18 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './HeroBanner.css';
 
 function HeroBanner(){
 
-    function handleLoad() {
-        const container = document.getElementById('hero-container');
-        container.onload = () => {
-            container.classList.add('loaded')
-        }
-        console.log(container);
-    }
+   
 
     return (
-        <div className='hero-container' onLoad={handleLoad}>
+        <div className={'hero-container'}>
             <div className='hero-title'>
                 <h1>John Tompkins.</h1>
                 <h2>Web Developer</h2> 
