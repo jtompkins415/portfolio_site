b1289311cf2fee6181765e71ec9c52ad3c53f75a
jtompkins415 <john.tompkins415@gmail.com>
2023-07-22 21:54:23 -0700

    add dotenv configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import dotenv from 'dotenv';
 // import './index.css'
 
+
+dotenv.config();
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-)
+);
