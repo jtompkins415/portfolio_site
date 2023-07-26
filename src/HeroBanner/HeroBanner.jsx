f1c8f3ab27282a0c5a64b00158a222ed70f50072
jtompkins415 <john.tompkins415@gmail.com>
2023-07-25 23:05:37 -0700

    Implement fade-in transition

--- src/HeroBanner/HeroBanner.jsx
+++ src/HeroBanner/HeroBanner.jsx
@@ -2,16 +2,21 @@ import React, {useState, useRef, useEffect} from 'react';
 import './HeroBanner.css';
 
 function HeroBanner(){
-
+    const [isVisible, setIsVisible] = useState(false);
+   
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
    
 
     return (
-        <div className={'hero-container'}>
-            <div className='hero-title'>
-                <h1>John Tompkins</h1>
+        <div className={`hero-container ${isVisible ? 'fade-in' : ''}`}>
+            <div className={`hero-title ${isVisible ? 'fade-in-title' : ''}`}>
+                <h1 className={`hero-title ${isVisible ? 'fade-in' : ''}`}>John Tompkins</h1>
             </div>
-            <div className='hero-subtitle'>
-                 <h2>Web Developer</h2>
+            <div className={`verticle-line ${isVisible ? 'fade-in-line' : ''}`}></div>
+            <div className={`hero-subtitle ${isVisible ? 'fade-in-subtitle' : ''}`}>
+                 <h2>Frontend Developer</h2>
                 <h2>Designer</h2> 
             </div>    
         </div>
