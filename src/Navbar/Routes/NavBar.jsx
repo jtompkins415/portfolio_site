3a0472523ccbb33238bef7dfd9f4cda567f62558
jtompkins415 <john.tompkins415@gmail.com>
2023-07-25 23:05:59 -0700

    implement multi color change transitions

--- src/Navbar/Routes/NavBar.jsx
+++ src/Navbar/Routes/NavBar.jsx
@@ -15,33 +15,33 @@ function NavBar(){
                 <div className='navlink-container'>
                   <Link 
                   href='#' 
-                  className='nav-link'
+                  className='nav-link orange'
                   underline='none'
                   >About</Link>
                   </div>
                   <div className='navlink-container'>
                   <Link 
                   href='#' 
-                  className='nav-link'
+                  className='nav-link blue'
                   underline='none'
                   >Projects</Link>
                   </div>
                   <div className='navlink-container'>
                   <Link 
                   href='#' 
-                  className='nav-link'
+                  className='nav-link green'
                   underline='none'
                   >Contact</Link> 
                 </div>
                 <div className='navlink-container-social'>
                   <Link 
                   href='https://github.com/jtompkins415' className='nav-link-social' underline='none'
-                  ><img src='src/assets/github-mark/github-mark-white.png' alt='github-logo' width={45}/></Link>
+                  ><img src='src/assets/github-mark/github-mark.png' alt='github-logo' width={45}/></Link>
                   <Link 
                   href='https://www.linkedin.com/in/jtompkins415/' 
                   className='nav-link-social'
                   underline='none'
-                  ><img className='nav-social-img' src='src/assets/LinkedIn_logo.png' width={45} style={{ filter: 'hue-rotate(270deg)' }}/></Link>
+                  ><img className='nav-social-img' src='src/assets/Black-linkedin-logo-design-on-transparent-background-PNG.png' width={45} style={{ filter: 'hue-rotate(270deg)' }}/></Link>
                 </div>
                 
             </div>
