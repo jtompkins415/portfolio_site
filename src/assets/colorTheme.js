3b7b38dc49de066156a2a15469d7b0dec5261158
jtompkins415 <john.tompkins415@gmail.com>
2023-07-27 20:25:17 -0700

    create color objects

--- src/assets/colorTheme.js
+++ src/assets/colorTheme.js
@@ -3,13 +3,20 @@ import { createTheme } from '@mui/material';
 
 const theme = createTheme({
     palette: {
-        primary: {
-            main: '#E5B728',
-            light: '#DBFFFA',
-            dark: '#62948D',
-            contrast_light: '#E0BBAB',
-            contrast_dark: '#946653',
+        green: {
+            main: '#119E2B',
+            light: '#1AEB40',
+            dark: '#0C6B1D',
+            contrast_light: '#47AE5A',
+            contrast_dark: '#60EB7A',
         },
+        red: {
+            main: '',
+            light: '',
+            dark: '',
+            contrast_light: '',
+            contrast_dark: ''
+        }
 
     },
 })
