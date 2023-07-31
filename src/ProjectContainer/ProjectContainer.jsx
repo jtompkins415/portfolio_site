0901e784ff6ec29b3dcdc0ba0fe94bf54c61dcf9
jtompkins415 <john.tompkins415@gmail.com>
2023-07-30 20:50:00 -0700

    implement fade in transition

--- src/ProjectContainer/ProjectContainer.jsx
+++ src/ProjectContainer/ProjectContainer.jsx
@@ -1,18 +1,30 @@
+import {useState, useRef, useEffect} from 'react';
 import Project from "./Project";
 import { projectObjectArray } from "./projectObjectArray";
 import './ProjectContainer.css';
 
 function ProjectContainer(){
+    const [isVisible, setIsVisible] = useState(false);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+   
+
+    const handleOnLoad = () => {
+        setIsImageLoaded(true);
+    }
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
 
     return(
-        <div className="main-container">
-            <div className="title-container">
+        <div className={`main-container ${isVisible ? 'fade-in' : ''}`}>
+            <div className={`title-container ${isVisible ? 'fade-in' : ''}`}>
                <h2>Projects</h2> 
             </div>
-            <div className="project-content-container">
-                <Project projectObject={projectObjectArray[0]} />
-                <Project projectObject={projectObjectArray[1]} />
-                <Project projectObject={projectObjectArray[2]} />
+            <div className={`project-content-container ${isVisible ? 'fade-in' : ''}`}>
+                <Project projectObject={projectObjectArray[0]} handleOnLoad={handleOnLoad} />
+                <Project projectObject={projectObjectArray[1]} handleOnLoad={handleOnLoad} />
+                <Project projectObject={projectObjectArray[2]} handleOnLoad={handleOnLoad} />
             </div>
         </div>
     )
