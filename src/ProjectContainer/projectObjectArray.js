14b7741171a3b248a63998b220f31f62ef106444
jtompkins415 <john.tompkins415@gmail.com>
2023-07-31 13:46:15 -0700

    edit project url

--- src/ProjectContainer/projectObjectArray.js
+++ src/ProjectContainer/projectObjectArray.js
@@ -4,7 +4,7 @@ export const projectObjectArray = [
     description: 'LOCAL PROVISIONS is an single page application, inspired by EATER/THRILLIST, that allows users to discover new restaurants, bars, and specialty food/beverage shops. A user can register for the site, explore places of interest in different regions, and save them to be referenced later.',
     techStack: ['React', 'Bootstrap', 'Express.js', 'PostgreSQL' ],
     imgSrc: '/assets/project-pics/localProvisions.jpeg',
-    url: 'https://github.com/jtompkins415/capstone-local-provisions'},
+    url: 'https://localprovisions.netlify.app/main'},
     {
         name: 'PROVISIONS: Online Storefront',
         description: 'PROVISIONS is an online platform providing users access to a highly currated selection of craft beer, natural wine, N/A beverages, and specialty food items.',
