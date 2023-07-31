9d41faa97af0e75d1c667ed42583b321c7a21234
jtompkins415 <john.tompkins415@gmail.com>
2023-07-31 10:40:42 -0700

    add icon for clear UI

--- src/HeroBanner/HeroBanner.jsx
+++ src/HeroBanner/HeroBanner.jsx
@@ -1,4 +1,5 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import './HeroBanner.css';
 
 function HeroBanner(){
@@ -10,18 +11,24 @@ function HeroBanner(){
    
 
     return (
-        <div className={`hero-container ${isVisible ? 'fade-in' : ''}`}>
-            <div className={`hero-title ${isVisible ? 'fade-in-title' : ''}`}>
-                <h1 className={`hero-title ${isVisible ? 'fade-in-title' : ''}`}>John Tompkins</h1>
+        <div className='hero-banner-container'>
+            <div className={`hero-container ${isVisible ? 'fade-in' : ''}`}>
+                <div className={`hero-title ${isVisible ? 'fade-in-title' : ''}`}>
+                    <h1 className={`hero-title ${isVisible ? 'fade-in-title' : ''}`}>John Tompkins</h1>
+                </div>
+                <div className={`hero-subtitle ${isVisible ? 'fade-in-subtitle' : ''}`}>
+                    <h2>Frontend Developer</h2>
+                    <div className={`horizontal-line ${isVisible ? 'fade-in-line' : ''}`}></div> 
+                </div>
+                <div className={`hero-img-container ${isVisible ? 'fade-in-img' : ''}`}>
+                    <img src='src/assets/newProfPic.jpg'/>
+                </div>
             </div>
-            <div className={`hero-subtitle ${isVisible ? 'fade-in-subtitle' : ''}`}>
-                 <h2>Frontend Developer</h2>
-                 <div className={`horizontal-line ${isVisible ? 'fade-in-line' : ''}`}></div> 
-            </div>
-            <div className={`hero-img-container ${isVisible ? 'fade-in-img' : ''}`}>
-                <img src='src/assets/newProfPic.jpg'/>
-            </div>    
+            <div className={`hero-icon-container ${isVisible ? 'fade-in-icon' : ''}`}>
+                <ArrowDropDownIcon style={{fontSize: '4rem'}} />
+            </div>  
         </div>
+       
     )
 };
 
